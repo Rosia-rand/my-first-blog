e1188b1573f70564990e334e9ee2476e23ac5a09
Rosia_rand <rosiarandria@gmail.com>
2021-12-15 11:29:59 +0300

    Ajout de la vue et du template de détail blog post ainsi que du CSS pour le site.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
