c240d11fd56db524181e602e618af6730a5e2cc0
Rosia_rand <rosiarandria@gmail.com>
2021-12-15 17:04:28 +0300

    Added views to create/edit blog post inside the site.

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
-from django.shortcuts import render, get_object_or_404
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {})
@@ -22,9 +21,6 @@ def post_new(request):
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})    
-def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
